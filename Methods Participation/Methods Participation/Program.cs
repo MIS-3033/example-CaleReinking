fc12c1fe0855dac058940e1b2ea44172f72fe6b4
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-08-26 12:03:25 -0500

    Worked on methods

--- Methods Participation/Methods Participation/Program.cs
+++ Methods Participation/Methods Participation/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Cale J Reinking
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Methods_Participation
 {
@@ -7,8 +8,18 @@ namespace Methods_Participation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cale is home");
 
+            double num1 = 2;
+                double num2 = 5;
+            var name = Console.ReadLine();
+            var summation = Add(num1, num2);
+            Console.WriteLine(summation.ToString("C5"));
         }
+        public static double Add(double firstnumber, double secondnumber)
+        {
+            double sum = 0;
+            sum = firstnumber + secondnumber;
+        }
+            
     }
 }
