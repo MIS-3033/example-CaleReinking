421012d24023988dca143c4b3d902093f5e2f404
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-09-02 12:18:34 -0500

    did it

---
+++ ConsoleApp1/StudentUMLExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace StudentUMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            Console.WriteLine(myStudent);
+            myStudent.FirstName = "Cale";
+            myStudent.LastName = "Reinking";
+            myStudent.IsOnProbation = true;
+            myStudent.SoonerID = 5;
+
+            Console.WriteLine(myStudent);
+            Console.WriteLine();
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+            Console.WriteLine();
+            myStudent.MakePayment(300);
+            Console.WriteLine(myStudent);
+        }
+    }
+}
---
+++ ConsoleApp1/StudentUMLExample/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentUMLExample
+{
+    class Student
+    {
+        //Hold alt + Shift to write on multiple lines
+        public int SoonerID { get; set; }
+       public string FirstName          { get; set; }
+         public string LastName             { get; set; }
+           public bool IsOnProbation          { get; set; }
+           public double GPA                  { get; set; }
+        //Do not need get set for private
+        private double BursarBalance;
+        
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 1000;
+
+        }
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+        public void MakePayment(double amount)
+        {
+            if (amount > 0)
+            {
+                BursarBalance = BursarBalance - amount;
+                 // BursarBalance -= amount;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        /// <summary>
+        /// Made to check balance
+        /// </summary>
+        /// <returns></returns>
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+        //Overriding creates a string representation of our instance of the class
+        public override string ToString()
+        {
+            string output = $"{FirstName} {LastName} {SoonerID}";
+            if (IsOnProbation == true)
+            {
+                output += "is on probation.\n";
+            }
+            else
+            {
+                output += "is not on probation.\n"
+            }
+            output += $"They have a {GPA.ToString("N2")}";
+            SoonerID = "";
+            FirstName
+            LastName
+            IsOnProbation
+            GPA = 0;
+            return base.ToString();
+        }
+    }
+}
